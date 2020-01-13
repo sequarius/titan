e6cf3d93afa3dc664dcbe846fe025df29d08aaf6
sequarius <sequarius@gmail.com>
2020-01-14 00:22:47 +0800

    feature: add IgnoreDateTimePlugin

--- titan-generator/src/main/java/com/sequarius/generator/mybatis/plugins/IgnoreAutoDatePlugin.java
+++ titan-generator/src/main/java/com/sequarius/generator/mybatis/plugins/IgnoreAutoDatePlugin.java
@@ -2,7 +2,6 @@ package com.sequarius.generator.mybatis.plugins;
 
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
-import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.VisitableElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
@@ -24,7 +23,6 @@ public class IgnoreAutoDatePlugin extends PluginAdapter {
     }
     @Override
     public boolean sqlMapUpdateByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
-//        removeTimeTextAttribute(element);
         removeTimeTextAttribute(element);
         return super.sqlMapUpdateByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
     }
@@ -36,27 +34,16 @@ public class IgnoreAutoDatePlugin extends PluginAdapter {
 
     private void removeTimeTextAttribute(XmlElement element) {
         List<VisitableElement> elements = element.getElements();
-        int removeCommaIndex=0;
-        TextElement replaceElement=null;
         Iterator<VisitableElement> iterator = elements.iterator();
         while (iterator.hasNext()){
             VisitableElement baseElements=iterator.next();
-            if (baseElements instanceof TextElement){
-                TextElement targetElment=(TextElement)baseElements;
-                if(targetElment.getContent().contains("update_time")||targetElment.getContent().contains("create_time")){
-                    iterator.remove();
-                }
+            if (!(baseElements instanceof TextElement)){
+                continue;
             }
-            if (baseElements instanceof TextElement){
-                TextElement targetElment=(TextElement)baseElements;
-                if(targetElment.getContent().contains("where")){
-                    removeCommaIndex=1;
-                    replaceElement=(TextElement)(elements.get(removeCommaIndex));
-                    String content = replaceElement.getContent().trim();
-                    replaceElement=new TextElement(content.substring(0,content.length()-1));
-                }
+            TextElement targetElement=(TextElement)baseElements;
+            if(targetElement.getContent().contains("update_time")||targetElement.getContent().contains("create_time")){
+                iterator.remove();
             }
-            iterator.next();
         }
     }
 
@@ -72,18 +59,26 @@ public class IgnoreAutoDatePlugin extends PluginAdapter {
             if (!(baseElements instanceof XmlElement)){
                 continue;
             }
-            XmlElement targetElment=(XmlElement)baseElements;
-            if(targetElment.getName().contains("set")){
-                List<VisitableElement> innerElements = targetElment.getElements();
-                for (int i1 = 0; i1 < innerElements.size(); i1++) {
-                    VisitableElement baseInnerElements=elements.get(i1);
-                    if (!(baseInnerElements instanceof XmlElement)){
+            XmlElement setTagElement=(XmlElement)baseElements;
+            if(!setTagElement.getName().contains("set")){
+                continue;
+            }
+            List<VisitableElement> setTagElements = setTagElement.getElements();
+            Iterator<VisitableElement> setTagIterator = setTagElements.iterator();
+            while (setTagIterator.hasNext()){
+                VisitableElement currentElement = setTagIterator.next();
+                if (!(currentElement instanceof XmlElement)){
+                    continue;
+                }
+                XmlElement setTagInnerElement=(XmlElement)currentElement;
+                for (VisitableElement next : setTagInnerElement.getElements()) {
+                    if (!(next instanceof TextElement)) {
                         continue;
                     }
-                    targetElment=(XmlElement)innerElements.get(i1);
-                    Iterator<Attribute> attributeIterator = targetElment.getAttributes().iterator();
-                    while (attributeIterator.hasNext()){
-                        attributeIterator.remove();
+                    TextElement textElement = (TextElement) next;
+                    if (textElement.getContent().contains("update_time") || textElement.getContent().contains("create_time")) {
+                        setTagIterator.remove();
+                        break;
                     }
                 }
             }
